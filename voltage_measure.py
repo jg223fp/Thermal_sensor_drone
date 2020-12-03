dea272e259099aa28ad9e05be838ea15a83efa5e
Pierre <pierre.oskarsson@me.com>
2020-12-03 11:35:43 +0100

    Updated ref value.

--- voltage_measure.py
+++ voltage_measure.py
@@ -2,10 +2,10 @@
 '''import machine
 
 adc = machine.ADC()             # create an ADC object
-raw_voltage = adc.channel(pin='P18', attn=adc.ATTN_11DB)        # create an analog pin on P13. 11DB to span over 2.1V.
+value = adc.channel(pin='P18', attn=adc.ATTN_11DB)        # create an analog pin on P13. 11DB to span over 2.198V.
 '''
 def voltage_measure(value):
-    vref = 2.1      #voltage reference.
+    vref = 2.198     #voltage reference. Calculated on 12.52V when battery is 100% charged.
     resolution_12bits = vref/4096       #(2^12 = 4096)
     Vin = value * resolution_12bits
     Vbat = Vin / (10000/(47000+10000))
