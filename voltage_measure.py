5aeb1be1d36933d2190ade1e97beae41e8e64932
Pierre <pierre.oskarsson@me.com>
2020-12-03 09:01:14 +0100

    Changed calculations.

--- voltage_measure.py
+++ voltage_measure.py
@@ -2,10 +2,11 @@
 '''import machine
 
 adc = machine.ADC()             # create an ADC object
-raw_voltage = adc.channel(pin='P13', attn=adc.ATTN_11DB)        # create an analog pin on P13. 11DB to span over 2.1V.
+raw_voltage = adc.channel(pin='P18', attn=adc.ATTN_11DB)        # create an analog pin on P13. 11DB to span over 2.1V.
 '''
-def voltage_measure(raw_voltage):
+def voltage_measure(value):
     vref = 2.1      #voltage reference.
     resolution_12bits = vref/4096       #(2^12 = 4096)
-    return raw_voltage * resolution_12bits
-    
+    Vin = value * resolution_12bits
+    Vbat = Vin / (10000/(47000+10000))
+    return Vbat
